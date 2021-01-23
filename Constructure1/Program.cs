abb8e44e666f367b3dba517a441056ba9e2d2cf3
mugetsu <mugetsu@MUGETSU>
2021-01-23 20:09:58 +0300

    Add project files.

---
+++ Constructure1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Constructure1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsrutureDictonary<int,string> consd = new ConsrutureDictonary<int ,string>();
+            consd.Add(3, "Hasan");
+        }
+    }
+}
